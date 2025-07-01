1db8899bb697241005df13eafb25883eb70f13dc
kok <74132215+Fyks@users.noreply.github.com>
2025-07-01 20:24:42 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -106,7 +106,7 @@ const cart = {
     if (checkoutItems) {
       checkoutItems.innerHTML = this.items.map(item => `
         <div class="checkout-item">
-          <span>${item.name}</span>
+          <span>${item.name} </span>
           <span>${item.price} руб.</span>
         </div>
       `).join('');
